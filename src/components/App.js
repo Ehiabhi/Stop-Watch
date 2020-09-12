8c51ce79c6284370d23ab53f4d48ffe12c229ee9
Ehiabhi <enimakpokpo@yahoo.com>
2020-09-12 02:48:42 +0100

    My first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Stopwatch from "./Stopwatch";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Stopwatch</h1>
+        <Stopwatch />
+      </div>
+    );
+  }
+}
+
+export default App;
