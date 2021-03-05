96674e408cb35469b1f32e46f7baedf9238528b9
Ehiabhi <enimakpokpo@yahoo.com>
2021-03-05 12:19:23 +0100

    Add a build folder

---
+++ build/precache-manifest.0dca29c166a54f9fa97c969e591690e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d494d0ce6104416d499d6f8d9811dbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d782053f582a38c772a5",
+    "url": "/static/js/2.6866b369.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6866b369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2f5fec3377f8977196",
+    "url": "/static/js/main.00f12ede.chunk.js"
+  },
+  {
+    "revision": "88712bed2421415114bd",
+    "url": "/static/js/runtime-main.cb811ce8.js"
+  }
+]);
